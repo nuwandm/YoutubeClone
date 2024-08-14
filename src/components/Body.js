84add370967f14da967cfce04289b6bd4176e7f9
Nuwan Madusanka <nuwandarshana2012@gmail.com>
2024-08-14 18:41:58 +0530

    created routers and added to dummy watch page

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,12 @@
 import React from "react";
 import SideBar from "./SideBar";
-import MainContainer from "./MainContainer";
+import { Outlet } from "react-router-dom";
 
 const Body = () => {
 	return (
-		<div className="grid grid-flow-col">
+		<div className="flex">
 			<SideBar />
-			<MainContainer />
+			<Outlet />
 		</div>
 	);
 };
---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+const GOOGLE_API_KEY = "AIzaSyBfzF6WewtGO96Y9yZhyKAQiT5aoFRmsMc";
+
+export const YOUTUBE_VIDEO_API =
+	"https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=LK&key=" +
+	GOOGLE_API_KEY;
