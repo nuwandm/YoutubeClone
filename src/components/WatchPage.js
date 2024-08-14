7f81e127f5ffd174b5c8e54fc6c95bac4d2a8f48
Nuwan Madusanka <nuwandarshana2012@gmail.com>
2024-08-14 19:01:41 +0530

    use iframe to open dynamic content

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -1,15 +1,30 @@
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { closeMenu } from "../utils/appSlice";
+import { useSearchParams } from "react-router-dom";
 
 const WatchPage = () => {
 	const dispatch = useDispatch();
+	const [searchParams, setSearchParms] = useSearchParams();
+	console.log(searchParams.get("v"));
 
 	useEffect(() => {
 		dispatch(closeMenu());
 	}, []);
 
-	return <div>inside watch</div>;
+	return (
+		<div className="px-5">
+			<iframe
+				width="900"
+				height="600"
+				src={`https://www.youtube.com/embed/${searchParams.get("v")}`}
+				title="YouTube video player"
+				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+				referrerpolicy="strict-origin-when-cross-origin"
+				allowfullscreen
+			></iframe>
+		</div>
+	);
 };
 
 export default WatchPage;
