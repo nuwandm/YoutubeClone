39c3c01a5b1d37dbe4b007e24be7fd252d587a3c
Nuwan Madusanka <nuwandarshana2012@gmail.com>
2024-08-18 22:24:29 +0530

    end-level nesting comment added

---
+++ src/components/CommentContainer.js
@@ -0,0 +1,148 @@
+import React from "react";
+import userIcon from "../Asserts/UserIcon.png";
+
+const commentsData = [
+	{
+		name: "Nuwan Madusanka",
+		comment: "Level 1 Comment",
+		replay: [
+			{
+				name: "Udara Maduranga",
+				comment: "Level 2 Reply",
+				replay: [],
+			},
+			{
+				name: "Udara Maduranga",
+				comment: "Level 2 Reply",
+				replay: [
+					{
+						name: "Nuwan Madusanka",
+						comment: "Level 3 Reply",
+						replay: [
+							{
+								name: "Nuwan Madusanka",
+								comment: "Level 4 Reply",
+								replay: [
+									{
+										name: "Nuwan Madusanka",
+										comment: "Level 5 Reply",
+										replay: [],
+									},
+								],
+							},
+						],
+					},
+				],
+			},
+			{
+				name: "Udara Maduranga",
+				comment: "Level 2 Reply",
+				replay: [
+					{
+						name: "Udara Maduranga",
+						comment: "Level 3 Reply",
+						replay: [],
+					},
+				],
+			},
+			{
+				name: "Udara Maduranga",
+				comment: "Level 2 Reply",
+				replay: [],
+			},
+		],
+	},
+	{
+		name: "Asintha Madushan",
+		comment: "Level 1 Comment",
+		replay: [
+			{
+				name: "Bhanuka Rajapakse",
+				comment: "Level 2 Reply",
+				replay: [],
+			},
+		],
+	},
+	{
+		name: "Nuwan Madusanka",
+		comment: "Level 1 Comment",
+		replay: [
+			{
+				name: "Udara Maduranga",
+				comment: "Level 2 Reply",
+				replay: [
+					{
+						name: "Nuwan Madusanka",
+						comment: "Level 3 Reply",
+						replay: [],
+					},
+				],
+			},
+			{
+				name: "Udara Maduranga",
+				comment: "Level 2 Reply",
+				replay: [],
+			},
+		],
+	},
+	{
+		name: "Dilan Perera",
+		comment: "Level 1 Comment",
+		replay: [
+			{
+				name: "Udara Maduranga",
+				comment: "Level 2 Reply",
+				replay: [
+					{
+						name: "Nuwan Madusanka",
+						comment: "Level 3 Reply",
+						replay: [],
+					},
+				],
+			},
+			{
+				name: "Udara Maduranga",
+				comment: "Level 2 Reply",
+				replay: [],
+			},
+		],
+	},
+];
+
+const Comment = ({ data }) => {
+	const { name, comment } = data;
+	return (
+		<div className="flex bg-gray-100 p-2 rounded-lg my-2">
+			<img className="w-10 h-10 flex " src={userIcon} alt="User" />
+			<div className="ml-2">
+				<p className="font-bold">{name}</p>
+				<p>{comment}</p>
+			</div>
+		</div>
+	);
+};
+
+const CommentList = ({ comments }) => {
+	return comments.map((comment, index) => (
+		<div>
+			<Comment key={index} data={comment} />
+			{comment?.replay && (
+				<div className="ml-5 pl-3 border border-l-black">
+					{/* Recursing CommentList */}
+					<CommentList key={index} comments={comment.replay} />
+				</div>
+			)}
+		</div>
+	));
+};
+
+const CommentContainer = () => {
+	return (
+		<div className="m-5 p-2">
+			<h1 className="text-2xl font-bold">Comments :</h1>
+			<CommentList comments={commentsData} />
+		</div>
+	);
+};
+
+export default CommentContainer;
--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { closeMenu } from "../utils/appSlice";
 import { useSearchParams } from "react-router-dom";
+import CommentContainer from "./CommentContainer";
 
 const WatchPage = () => {
 	const dispatch = useDispatch();
@@ -12,16 +13,19 @@ const WatchPage = () => {
 	}, []);
 
 	return (
-		<div className="px-5">
-			<iframe
-				width="900"
-				height="600"
-				src={`https://www.youtube.com/embed/${searchParams.get("v")}`}
-				title="YouTube video player"
-				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-				referrerpolicy="strict-origin-when-cross-origin"
-				allowfullscreen
-			></iframe>
+		<div className="flex flex-col">
+			<div className="px-5">
+				<iframe
+					width="900"
+					height="600"
+					src={`https://www.youtube.com/embed/${searchParams.get("v")}`}
+					title="YouTube video player"
+					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+					referrerpolicy="strict-origin-when-cross-origin"
+					allowfullscreen
+				></iframe>
+			</div>
+			<CommentContainer />
 		</div>
 	);
 };
